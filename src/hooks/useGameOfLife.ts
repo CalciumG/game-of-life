d467446eb593e9e0505f582ce96d50b08fae581f
CalciumG <52017802+CalciumG@users.noreply.github.com>
2023-12-22 22:00:35 +0000

    fix alive count

--- src/hooks/useGameOfLife.ts
+++ src/hooks/useGameOfLife.ts
@@ -6,6 +6,7 @@ import { operations } from "@/utils/gridUtils";
 type Grid = number[][];
 
 export default function useGameOfLife() {
+  const grid = useStore((state) => state.grid);
   const setGrid = useStore((state) => state.setGrid);
   const setAlive = useStore((state) => state.setAlive);
 
@@ -14,12 +15,11 @@ export default function useGameOfLife() {
 
   const nextStep = useCallback(
     (numRows: number, numCols: number): void => {
-      const newGrid = produce(useStore.getState().grid, (gridCopy: Grid) => {
+      const newGrid = produce(grid, (gridCopy: Grid) => {
         for (let row = 0; row < numRows; row++) {
           for (let col = 0; col < numCols; col++) {
             let neighbors = 0;
-            operations.forEach((operation) => {
-              const [x, y] = operation;
+            operations.forEach(([x, y]) => {
               const newRow = row + x;
               const newCol = col + y;
               if (
@@ -28,30 +28,30 @@ export default function useGameOfLife() {
                 newCol >= 0 &&
                 newCol < numCols
               ) {
-                neighbors += useStore.getState().grid[newRow][newCol];
+                neighbors += grid[newRow][newCol];
               }
             });
 
             if (neighbors < 2 || neighbors > 3) {
               gridCopy[row][col] = 0;
-            } else if (
-              useStore.getState().grid[row][col] === 0 &&
-              neighbors === 3
-            ) {
+            } else if (grid[row][col] === 0 && neighbors === 3) {
               gridCopy[row][col] = 1;
             }
           }
         }
       });
+
       setGrid(newGrid);
+      setAlive(countAlive(newGrid));
     },
-    [setGrid]
+    [grid, setGrid, setAlive]
   );
 
   const updateCell = (row: number, col: number): void => {
-    const newGrid = produce(useStore.getState().grid, (gridCopy: Grid) => {
-      gridCopy[row][col] = useStore.getState().grid[row][col] ? 0 : 1;
+    const newGrid = produce(grid, (gridCopy: Grid) => {
+      gridCopy[row][col] = grid[row][col] ? 0 : 1;
     });
+
     setGrid(newGrid);
     setAlive(countAlive(newGrid));
   };
